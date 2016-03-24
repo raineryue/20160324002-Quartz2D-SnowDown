8a3200131603b35933473548b45cefac8d8a12a9
raineryue <it_jinlei.yue@163.com>
2016-03-24 22:29:22 +0800

    Initial Commit

---
+++ 20160324002-Quartz2D-SnowDown/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160324002-Quartz2D-SnowDown
+//
+//  Created by Rainer on 16/3/24.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20160324002-Quartz2D-SnowDown/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160324002-Quartz2D-SnowDown
+//
+//  Created by Rainer on 16/3/24.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
